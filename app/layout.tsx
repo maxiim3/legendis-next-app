d82b6d557c8fa55f11fdfaaa4a542090ba0f9fc9
Maxime Tamburrini <maximemickael.contact@gmail.com>
2023-12-19 19:47:45 +0100

    New carousel widget with 3d FX

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,26 @@
-import Footer from '@/components/organisms/footer.client';
-import Header from '@/app/header';
-import { italianno,roboto,tangerine } from '@/styles/fonts';
-import type { Metadata } from 'next';
-import Image from 'next/image';
-import React from 'react';
-import './globals.css';
+import Header from "@/app/header";
+import Footer from "@/components/organisms/footer.client";
+import {italianno, roboto, tangerine} from "@/styles/fonts";
+import type {Metadata} from "next";
+import React from "react";
+import "./globals.css";
 
 export const metadata: Metadata = {
-   title: 'Legendis',
-   description: 'Un label Jamais 203 Productions',
+    title: "Legendis",
+    description: "Un label Jamais 203 Productions",
 };
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
-   return (
-      <html
-         data-theme={'legendisTheme'}
-         lang='en'>
-         <body
-            className={`relative ${roboto.className} ${italianno.className} ${tangerine.className}`}>
-            <Header />
-            {children}
-            <Footer />
-         </body>
-      </html>
-   );
+    return (
+        <html
+            data-theme={"legendisTheme"}
+            lang='en'>
+            <body
+                className={`relative ${roboto.className} ${italianno.className} ${tangerine.className}`}>
+                <Header />
+                {children}
+                <Footer />
+            </body>
+        </html>
+    );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,142 +1,187 @@
-import { CarouselNavigation } from '@/app/carouselNavigation';
-import { AlbumFactory } from '@/lib/sanity/album';
-import ImageBuilder from '@/lib/sanity/image.builder';
-import { getAllAlbums } from '@/lib/sanity/service';
-import { cn } from '@/lib/utils';
-import React,{ Suspense,type ComponentPropsWithoutRef } from 'react';
+import {CarouselNavigation} from "@/app/carouselNavigation";
+import CarouselWidget from "@/components/organisms/carousel-widget";
+import {AlbumFactory} from "@/lib/sanity/album";
+import ImageBuilder from "@/lib/sanity/image.builder";
+import {getAllAlbums} from "@/lib/sanity/service";
+import {cn} from "@/lib/utils";
+import React, {Suspense, type ComponentPropsWithoutRef} from "react";
 
 export default async function HomePage() {
-   const albums: Awaited<ReturnType<typeof AlbumFactory>[]> = await getAllAlbums();
-   return (
-      // <main className='mt-24'>
-      <main>
-         <AlbumSlideShow albums={albums} />
-         <ArtistPresentation />
-         <ContactCTA />
-      </main>
-   );
+    const albums: Awaited<ReturnType<typeof AlbumFactory>[]> =
+        await getAllAlbums();
+    return (
+        // <main className='mt-24'>
+        <main>
+            <header className='absolute left-0 top-3 z-30 w-full pt-8 text-4xl text-primary'>
+                <h1 className='mx-auto text-center'>LEGENDIS</h1>
+            </header>
+            <AlbumSlideShow albums={albums} />
+            <ArtistPresentation />
+            <CarouselWidget albums={albums} />
+            <ContactCTA />
+        </main>
+    );
 }
 
-function PageSection({children, className, id}: ComponentPropsWithoutRef<'section'>) {
-   return (
-      <section
-         id={id}
-         className={cn('min-h-screen w-screen bg-base-200', className)}>
-         {children}
-      </section>
-   );
+function PageSection({
+    children,
+    className,
+    id,
+}: ComponentPropsWithoutRef<"section">) {
+    return (
+        <section
+            id={id}
+            className={cn("min-h-screen w-screen bg-base-200", className)}>
+            {children}
+        </section>
+    );
 }
 
 function AlbumSlideShow({albums}: {albums: ReturnType<typeof AlbumFactory>[]}) {
-   return (
-      <PageSection
-         className={'relative'}
-         id='albums'>
-         {/* <header className=' absolute top-0 flex w-full justify-center'> */}
-         {/*    <Heading2>ALBUMS</Heading2> */}
-         {/* </header> */}
-         {/*<div className='carousel h-[calc(100vh-96px)] w-screen border bg-base-300'>*/}
-         <div className='carousel h-screen w-screen border bg-base-300'>
-            <Suspense
-               fallback={
-                  <>
-                     <AlbumSkeleton />
-                     <AlbumSkeleton />
-                     <AlbumSkeleton />
-                  </>
-               }>
-               {albums.map((album, index) => {
-                  const date = album.releaseDate && new Date(album?.releaseDate);
-                  const year = date && date.getFullYear();
-                  const imageSource = ImageBuilder(album.album_cover).url();
+    return (
+        <PageSection
+            className={"relative"}
+            id='albums'>
+            {/* <header className=' absolute top-0 flex w-full justify-center'> */}
+            {/*    <Heading2>ALBUMS</Heading2> */}
+            {/* </header> */}
+            {/*<div className='carousel h-[calc(100vh-96px)] w-screen border bg-base-300'>*/}
+            <div className='carousel h-screen w-screen border bg-base-300'>
+                <Suspense
+                    fallback={
+                        <>
+                            <AlbumSkeleton />
+                            <AlbumSkeleton />
+                            <AlbumSkeleton />
+                        </>
+                    }>
+                    {albums.map((album, index) => {
+                        const date =
+                            album.releaseDate && new Date(album?.releaseDate);
+                        const year = date && date.getFullYear();
+                        const imageSource = ImageBuilder(
+                            album.album_cover
+                        ).url();
 
-                  return (
-                     <AlbumCardWrapper
-                        id={`album-${index}`}
-                        key={index}>
-                        <img
-                           className=' top-0 flex h-full w-full object-cover'
-                           src={imageSource}
-                        />
-                        <AlbumDetailsWrapper>
-                           {album.interprete && <Interpreters interpreters={album.interprete} />}
-                           <AlbumTitle>{album.title!.fr}</AlbumTitle>
-                           {/*<ListDetails*/}
-                           {/*   items={album?.compositeur}*/}
-                           {/*   title={'Compositeur'}*/}
-                           {/*/>*/}
-                           {year && <DisplayYear year={year} />}
-                        </AlbumDetailsWrapper>
-                     </AlbumCardWrapper>
-                  );
-               })}
-               <CarouselNavigation len={albums.length - 1} />
-            </Suspense>
-         </div>
-      </PageSection>
-   );
+                        return (
+                            <AlbumCardWrapper
+                                id={`album-${index}`}
+                                key={index}>
+                                <img
+                                    className=' top-0 flex h-full w-full object-cover'
+                                    src={imageSource}
+                                />
+                                <AlbumDetailsWrapper>
+                                    {album.interprete && (
+                                        <Interpreters
+                                            interpreters={album.interprete}
+                                        />
+                                    )}
+                                    <AlbumTitle>{album.title!.fr}</AlbumTitle>
+                                    {/*<ListDetails*/}
+                                    {/*   items={album?.compositeur}*/}
+                                    {/*   title={'Compositeur'}*/}
+                                    {/*/>*/}
+                                    {year && <DisplayYear year={year} />}
+                                </AlbumDetailsWrapper>
+                            </AlbumCardWrapper>
+                        );
+                    })}
+                    <CarouselNavigation len={albums.length - 1} />
+                </Suspense>
+            </div>
+        </PageSection>
+    );
 }
 
-function AlbumCardWrapper({children, id}: ComponentPropsWithoutRef<'div'>) {
-   return (
-      <div
-         id={id}
-         className=' carousel-item relative mx-auto h-full w-full rounded-box'>
-         {children}
-      </div>
-   );
+function AlbumCardWrapper({children, id}: ComponentPropsWithoutRef<"div">) {
+    return (
+        <div
+            id={id}
+            className=' carousel-item relative mx-auto h-full w-full rounded-box'>
+            {children}
+        </div>
+    );
 }
 
-const AlbumSkeleton = () => <AlbumCardWrapper />;
-
-function AlbumDetailsWrapper({children}: ComponentPropsWithoutRef<'article'>) {
-   return (
-      <article className='absolute bottom-0 flex w-full flex-col  items-center gap-2 px-2 py-12 '>
-         {children}
-      </article>
-   );
+function AlbumSkeleton() {
+    return <AlbumCardWrapper />;
 }
 
-function AlbumTitle({children}: ComponentPropsWithoutRef<'p'>) {
-   return <h3 className={'text-center text-xl uppercase text-balance md:text-2xl'}>{children}</h3>;
+function AlbumDetailsWrapper({children}: ComponentPropsWithoutRef<"article">) {
+    return (
+        <article className='absolute bottom-0 flex w-full flex-col  items-center gap-2 px-2 py-12 '>
+            {children}
+        </article>
+    );
+}
+
+function AlbumTitle({children}: ComponentPropsWithoutRef<"p">) {
+    return (
+        <h3
+            className={
+                "text-center text-xl uppercase text-balance md:text-2xl"
+            }>
+            {children}
+        </h3>
+    );
 }
 
 function Interpreters({interpreters}: {interpreters: string[]}) {
-   return (
-      <ListDetails
-         items={interpreters}
-         title={null}
-      />
-   );
+    return (
+        <ListDetails
+            items={interpreters}
+            title={null}
+        />
+    );
 }
 
-function ListDetails({items, title}: {items: string[] | undefined; title: string | null}) {
-   return (
-      items && (
-         <>
-            {title && <h4 className={'m-1 p-0 text-lg uppercase md:text-xl'}>{title}</h4>}
-            <ul className={'prose m-1 flex flex-col items-center justify-center p-0'}>
-               {items
-                  .filter(item => item.length)
-                  .map((item, idx) => (
-                     <li
-                        key={idx}
-                        className={'m-0 list-none p-0'}>
-                        {item}
-                     </li>
-                  ))}
-            </ul>
-         </>
-      )
-   );
+function ListDetails({
+    items,
+    title,
+}: {
+    items: string[] | undefined;
+    title: string | null;
+}) {
+    return (
+        items && (
+            <>
+                {title && (
+                    <h4 className={"m-1 p-0 text-lg uppercase md:text-xl"}>
+                        {title}
+                    </h4>
+                )}
+                <ul
+                    className={
+                        "prose m-1 flex flex-col items-center justify-center p-0"
+                    }>
+                    {items
+                        .filter(item => item.length)
+                        .map((item, idx) => (
+                            <li
+                                key={idx}
+                                className={"m-0 list-none p-0"}>
+                                {item}
+                            </li>
+                        ))}
+                </ul>
+            </>
+        )
+    );
 }
 
-const DisplayYear = ({year}: {year: number}) => <p>{year}</p>;
+function DisplayYear({year}: {year: number}) {
+    return <p>{year}</p>;
+}
 
 function ArtistPresentation() {
-   return <PageSection id='team'>ARTISTS</PageSection>;
+
+
+    return <PageSection id='team'>
+        COUCOU
+    </PageSection>;
 }
 
 function ContactCTA() {
-   return <PageSection>CONTACT</PageSection>;
+    return <PageSection>CONTACT</PageSection>;
 }
